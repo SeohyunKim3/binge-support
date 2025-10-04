0261588b7419c8b8795ef1d4616c54664718aecc
Seohyun Kim <ksh030208@snu.ac.kr>
2025-10-04 20:28:38 +0900

    update dashboard and social page

--- binge-support/src/app/transition-provider.tsx
+++ binge-support/src/app/transition-provider.tsx
@@ -16,7 +16,6 @@ export default function TransitionProvider({
         key={pathname}
         initial={{ opacity: 0, y: 2 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -2 }}
         transition={{
           duration: 0.2,
           ease: [0.33, 1, 0.68, 1],
